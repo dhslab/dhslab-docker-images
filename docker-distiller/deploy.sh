9f0cdf9248b3aebb01e9dd1535c37d6671baad4e
dhspence <4720638+dhspence@users.noreply.github.com>
2023-02-10 15:14:21 -0600

    new hic docker

--- docker-distiller/deploy.sh
+++
@@ -1,8 +0,0 @@
-echo "$DOCKER_PASSWORD" | docker login -u open2c --password-stdin
-
-version=$(cat ../VERSION | sed -nre 's/^[^0-9]*(([0-9]+\.)*[0-9]+).*/\1/p')
-echo "version: $version"
-
-# push it
-docker push open2c/distiller_env:latest
-docker push open2c/distiller_env:$version
